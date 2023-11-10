921c7707cd6b24bc81b271aae67cd7886bb927d4
JONATHAN HICKS <jonny@teachon.org>
2023-11-10 16:10:32 +0700

    create dummy book objects and loop function to add

--- script.js
+++ script.js
@@ -10,16 +10,57 @@ To facilitate this you will want to create the function that toggles a book’s 
 */
 
 const libraryContainer = document.querySelector(".library-container");
+const title = document.querySelector(".title");
+const author = document.querySelector(".author");
+const pages = document.querySelector(".pages");
+const readStatus = document.querySelector(".read-status");
 
 const myLibrary = [];
 
-function Book(name, author, numPages, haveRead) {
-  this.name = name;
+function Book(title, author, numPages, readStatus) {
+  this.title = title;
   this.author = author;
   this.numPages = numPages;
-  this.haveRead = haveRead;
+  this.readStatus = readStatus;
+  this.info = function () {
+    return `${name} by ${author}, ${numPages}, ${
+      readStatus === "yes" ? "have read" : "have not read yet."
+    }`;
+  };
 }
+
+const lordOfTheRings = new Book("Lord of the Rings", "JR Tolkien", 300, "yes");
+const theHobbit = new Book("The Hobbit", "JR Tolkien", 300, "no");
+const breakfastOfChampions = new Book(
+  "Breakfast of Champions",
+  "Kurt Vonnegut",
+  200,
+  "yes"
+);
+const catsCradle = new Book("Cat's Cradle", "Kurt Vonnegut", 250, "yes");
 
 function addBookToLibrary(book) {
   myLibrary.push(book);
 }
+
+addBookToLibrary(lordOfTheRings);
+addBookToLibrary(theHobbit);
+addBookToLibrary(breakfastOfChampions);
+addBookToLibrary(catsCradle);
+
+myLibrary.forEach((book) => {
+  const bookContainer = document.createElement("div");
+  const title = document.createElement("h2");
+  const author = document.createElement("p");
+  const pages = document.createElement("p");
+  const readStatus = document.createElement("button");
+  title.textContent = book.title;
+  author.textContent = book.author;
+  pages.textContent = book.numPages;
+  readStatus.textContent = book.readStatus;
+  libraryContainer.appendChild(bookContainer);
+  bookContainer.appendChild(title);
+  bookContainer.appendChild(author);
+  bookContainer.appendChild(pages);
+  bookContainer.appendChild(readStatus);
+});
